8ce9da0449b3dfafc7a061acf12c86cb00d4f38b
Ernesto Arellano <ernestoarellano@icloud.com>
2022-07-11 19:56:06 -0700

    upgrade packages, remove firebase and add GitHub provider to sign-in

--- interfaces/Firebase.ts
+++
@@ -1,5 +0,0 @@
-import { FirebaseApp } from 'firebase/app'
-
-export interface Firebase {
-  firebase: FirebaseApp
-}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
 import { prisma } from 'lib/prisma'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GitHubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     CredentialsProvider({
       name: 'credentials',
       credentials: {
